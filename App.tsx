68abad0a10f9089d57712e18f9ddf9f362228501
KseniiaKost <131806539+KseniiaKost@users.noreply.github.com>
2024-05-01 12:59:22 +0100

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Characters from "./components/characters/Characters";
+import Recipes from "./components/recipes/Recipes";
+import Products from "./components/products/Products";
+
+const App = () => {
+  return (
+    <>
+        <Products></Products>
+      </>
+  );
+}
+
+export default App;
