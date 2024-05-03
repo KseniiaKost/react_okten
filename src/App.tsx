02631656f49b0098773d6ef504491208d7b1d064
KseniiaKost <KKostyuchenko18@gmail.com>
2024-05-03 12:21:48 +0100

    HW1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Products from "./components/products/Products";
+
 
 
 const App = () => {
-  return (
-      <div>
-
-      </div>
-  );
-};
+    return (
+        <>
+            <Products></Products>
+        </>
+    );
+}
 
 export default App;
 
---
+++ src/components/products/Products.tsx
@@ -0,0 +1,39 @@
+import React, {FC, useEffect, useState} from 'react';
+import Product, {IProductProps} from "../product/Product";
+
+const Products: FC = () => {
+
+    const[product, setProduct] = useState<IProductProps[]>([]);
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/products')
+            .then((res) => res.json())
+            .then( ({products}) =>
+                setProduct(products))
+    }, []);
+
+
+    return (
+        <div>
+            {
+                product.map((value:IProductProps) => (
+                    <Product key={value.id}
+                             id={value.id}
+                             title={value.title}
+                             description={value.description}
+                             price={value.price}
+                             discountPercentage={value.discountPercentage}
+                             rating={value.rating}
+                             stock={value.stock}
+                             brand={value.brand}
+                             category={value.category}
+                             thumbnail={value.thumbnail}
+                             images={value.images}
+                    />
+                ))}
+
+        </div>
+    );
+};
+
+export default Products;
